75014d019c681facd08c705d932f4ad13efb52e1
kunal932 <kunal@staragrifinance.com>
2020-10-18 16:51:13 +0530

    media folder added, and 5 webapp created named category,payment,user, product,order all inside api named webapp

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
